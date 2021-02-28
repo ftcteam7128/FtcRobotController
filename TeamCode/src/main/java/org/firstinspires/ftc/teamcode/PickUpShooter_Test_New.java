e24097ba005db7a57b0320d6b799af4a25b5514b
Ishani Das <ishanidas05@gmail.com>
2021-02-28 14:39:25 -0800

    Updated pick up/shooter code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUpShooter_Test_New.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUpShooter_Test_New.java
@@ -10,35 +10,46 @@ import com.qualcomm.robotcore.util.Range;
 // import com.qualcomm.robotcore.hardware.Servo.MAX_POSITION;
 
 @TeleOp(name="Basic: PickUpShooter_Test", group="Iterative Opmode")
-public class PickUpShooter_Test_New extends LinearOpMode{
+public class PickUpShooter_Test_New extends LinearOpMode {
+
+    // need to add motor that controls slide below (silding taken care of by encoders)
+    // full unit up - switch is off -> encoder = 0 -> bring down before pick up
+    // move full unit at once
 
     DcMotor rightIntakeMotor;
-    Servo intakeServo;
+    Servo shootServo;
     DcMotor shooterMotor;
 
     // double ENCODER_TICKS_PER_INCH = ((28 * 40)/2.6)/(Math.PI*4);
     double ENCODER_TICKS_PER_REVOLUTION = 288;
 
-
-    public void runOpMode(){
+    public void runOpMode() {
         rightIntakeMotor = hardwareMap.get(DcMotor.class, "intake");
-        intakeServo = hardwareMap.get(Servo.class, "IntakeServo");
-        shooterMotor = hardwareMap.get(DcMotor.class, "shooter");
+        shootServo = hardwareMap.get(Servo.class, "shoot");
+        shooterMotor = hardwareMap.get(DcMotor.class, "shoot");
 
-        intakeServo.setPosition(0);
+        shootServo.setPosition(0.5);
 
+        /*
         rightIntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        int targetPos = (int) (rightIntakeMotor.getCurrentPosition() + (5*ENCODER_TICKS_PER_REVOLUTION));
+        int targetPos = (int) (rightIntakeMotor.getCurrentPosition() + (5 * ENCODER_TICKS_PER_REVOLUTION));
         rightIntakeMotor.setTargetPosition(targetPos);
         rightIntakeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightIntakeMotor.setPower(0.25f);
-        while(rightIntakeMotor.isBusy()){
+        while (rightIntakeMotor.isBusy()) {
             telemetry.addLine("Intaking");
             telemetry.update();
         }
         rightIntakeMotor.setPower(0);
+        */
 
-        while(opModeIsActive()){
+        waitForStart();
+
+        rightIntakeMotor.setPower(0.5f); // increased
+        sleep(5000);
+        rightIntakeMotor.setPower(0);
+
+        while (opModeIsActive()) {
             boolean push = gamepad2.x;
             boolean shoot = gamepad2.b;
 
@@ -47,81 +58,15 @@ public class PickUpShooter_Test_New extends LinearOpMode{
                 // intakeServo.setPosition(intakeServo.getPosition()+0.5);
                 telemetry.addLine("Pushing");
                 telemetry.update();
-                intakeServo.setPosition(0.5); // min: -1, max: 1
+                shootServo.setPosition(0.7); // min: -1, max: 1 // change back to 0.15
             }
             if (shoot) {
-                shooterMotor.setPower(0.25f);
+                shooterMotor.setPower(-1.0f);
+                sleep(5000);
+                shooterMotor.setPower(0);
             }
+
+            sleep(1000);
         }
     }
-/*
-    @Override
-    public void start() {
-        intakeServo.setPosition(0);
-
-        rightIntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        int targetPos = (int) (rightIntakeMotor.getCurrentPosition() + (5*ENCODER_TICKS_PER_REVOLUTION));
-        rightIntakeMotor.setTargetPosition(targetPos);
-        rightIntakeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightIntakeMotor.setPower(0.25f);
-        while(rightIntakeMotor.isBusy()){
-            telemetry.addLine("Intaking");
-            telemetry.update();
-        }
-        rightIntakeMotor.setPower(0);
-    }
-*/
-    @Override
-    public void loop(){
-
-        boolean push = gamepad2.x;
-        boolean shoot = gamepad2.b;
-
-        if (push) {
-            // intakeServo.setPosition(Range.clip(0.5, 0, 1));
-            // intakeServo.setPosition(intakeServo.getPosition()+0.5);
-            telemetry.addLine("Pushing");
-            telemetry.update();
-            intakeServo.setPosition(0.5); // min: -1, max: 1
-        }
-        if (shoot) {
-            shooterMotor.setPower(0.25f);
-            /*
-            shooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            int targetPos = (int) (shooterMotor.getCurrentPosition() + (2*ENCODER_TICKS_PER_REVOLUTION));
-            shooterMotor.setTargetPosition(targetPos);
-            shooterMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            shooterMotor.setPower(0.25f);
-            while(shooterMotor.isBusy()){
-                telemetry.addLine("Shooting");
-                telemetry.update();
-            }
-            shooterMotor.setPower(0);
-             */
-        }
-
-    }
-
-    // ---------------------------- Mech methods ----------------------------
-
-    public void push(){
-        // move servo 90 degrees
-        // intakeServo.setPosition(0.5);
-        intakeServo.setPosition(Range.clip(0.5, 0, 1));
-    }
-
-    public void shoot() {
-        shooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        int targetPos = (int) (shooterMotor.getCurrentPosition() + (2*ENCODER_TICKS_PER_REVOLUTION));
-        shooterMotor.setTargetPosition(targetPos);
-        shooterMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        shooterMotor.setPower(0.25f);
-        while(shooterMotor.isBusy()){
-            telemetry.addLine("Shooting");
-            telemetry.update();
-        }
-        shooterMotor.setPower(0);
-    }
-
-    // figure out how to add ring scanning
 }
