6085e101bd06aacc398862e5ffce25152181ba6c
Ishani Das <ishanidas05@gmail.com>
2021-02-28 15:25:49 -0800

    Re-organized pick up/shooter mech to also include base motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUpShooter_Test_New.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUpShooter_Test_New.java
@@ -19,29 +19,26 @@ public class PickUpShooter_Test_New extends LinearOpMode {
     DcMotor rightIntakeMotor;
     Servo shootServo;
     DcMotor shooterMotor;
+    DcMotor baseMotor;
+
+
+    // pick up angles
+    double zero_rings_angle = 0;
+    double one_rings_angle = 0;
+    double four_rings_angle = 0;
+
+    double shootAngle = 45;
 
     // double ENCODER_TICKS_PER_INCH = ((28 * 40)/2.6)/(Math.PI*4);
     double ENCODER_TICKS_PER_REVOLUTION = 288;
 
+    /*
     public void runOpMode() {
         rightIntakeMotor = hardwareMap.get(DcMotor.class, "intake");
         shootServo = hardwareMap.get(Servo.class, "shoot");
         shooterMotor = hardwareMap.get(DcMotor.class, "shoot");
 
         shootServo.setPosition(0.5);
-
-        /*
-        rightIntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        int targetPos = (int) (rightIntakeMotor.getCurrentPosition() + (5 * ENCODER_TICKS_PER_REVOLUTION));
-        rightIntakeMotor.setTargetPosition(targetPos);
-        rightIntakeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightIntakeMotor.setPower(0.25f);
-        while (rightIntakeMotor.isBusy()) {
-            telemetry.addLine("Intaking");
-            telemetry.update();
-        }
-        rightIntakeMotor.setPower(0);
-        */
 
         waitForStart();
 
@@ -65,6 +62,58 @@ public class PickUpShooter_Test_New extends LinearOpMode {
                 sleep(5000);
                 shooterMotor.setPower(0);
             }
+
+            sleep(1000);
+        }
+    }
+     */
+
+    public void runOpMode() {
+        rightIntakeMotor = hardwareMap.get(DcMotor.class, "intake");
+        shootServo = hardwareMap.get(Servo.class, "shoot");
+        shooterMotor = hardwareMap.get(DcMotor.class, "shoot");
+        baseMotor = hardwareMap.get(DcMotor.class, "base");
+
+        // use ring scanning to figure out how many rings are there
+        // set the final angle based on that (lets assume there's one ring for now)
+
+        double angle = one_rings_angle;
+
+        shootServo.setPosition(0.15);
+        baseMotor.setTargetPosition((int)angle);
+
+        waitForStart();
+
+        rightIntakeMotor.setPower(0.5f); // increased
+        sleep(5000);
+        rightIntakeMotor.setPower(0);
+
+        while (opModeIsActive()) {
+            boolean intake = gamepad2.x;
+            boolean shoot = gamepad2.b;
+
+            if (intake) {
+                // intakeServo.setPosition(Range.clip(0.5, 0, 1));
+                // intakeServo.setPosition(intakeServo.getPosition()+0.5);
+                telemetry.addLine("Intaking");
+                telemetry.update();
+                // shootServo.setPosition(0.7); // min: -1, max: 1 // change back to 0.15
+                rightIntakeMotor.setPower(0.5);
+
+                // move to shooting angle (45 deg)
+                // NEED TO FIX
+                baseMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                baseMotor.setTargetPosition((int)shootAngle);
+            }
+            else rightIntakeMotor.setPower(0);
+
+            if (shoot) {
+                shootServo.setPosition(0.7); // min: -1, max: 1 // change back to 0.15
+                shooterMotor.setPower(-1.0f);
+                sleep(5000);
+                shooterMotor.setPower(0);
+            }
+            else shootServo.setPosition(0.15);
 
             sleep(1000);
         }
